95bfbe2341f74d39d5feb4f275bb3ab133126e09
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-04-29 20:50:11 -0700

    Give the user the opportunity to update a key

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -27,3 +27,15 @@ client_details['like_modern'] = gets.chomp[0].downcase == 'y'
 
 client_details.each {|key, value| puts "#{key} is: #{value}" }
 
+
+puts 'Do you want to update any of your inputs (yes or none)?'
+
+if gets.chomp[0].downcase == 'y'
+  puts 'Which key you wanna update?'
+  key = gets.chomp
+  puts 'What is the new value?'
+  client_details[key] = gets.chomp
+end
+
+client_details.each {|key, value| puts "#{key} is: #{value}" }
+
