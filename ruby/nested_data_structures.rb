699f0ad92ff1dcbfa34db3012c4be5fa23d8d63f
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-01 00:24:49 -0700

    Designed and Build a Nested Data Structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,31 @@
+mandilco = {
+  departments: {
+    hr: { employees: 4,
+		      desks: 3,
+			    computers: 4
+        },
+    finance: {  employees: 8,
+			          desks: 3,
+			          computers: 7
+              }
+  },
+  vehicles: {
+    veh1: {
+      license_plate: '241e3',
+			type: 'SUV',
+			model: 'Ford'
+    },
+    veh2: {
+      license_plate: '2r1e3',
+			type: 'SUV',
+			model: 'Chevy'
+    }
+  },
+
+  machines: [
+    'machine1', 
+    'machine2', 
+    'machine3']
+}
+
+puts mandilco
