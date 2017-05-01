4176246f1876a2695d265cf039af82f605274b1e
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-01 08:33:29 -0700

    finished

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -11,16 +11,25 @@ zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
+
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
 # ----
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
+
 # ----
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
+
 # ----
 
 # 5. You found another survivor! This means you can combine your supplies.
@@ -47,16 +56,22 @@ extinct_animals = {
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 # ----
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -65,9 +80,13 @@ extinct_animals = {
 # "Saiga Antelope"
 # Do not use any special built-in methods.
 # ----
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+zombie_apocalypse_supplies.each {|supply| p "#{supply} *"}
+
