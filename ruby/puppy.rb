4166e031e90c39854c647662a77c9dbc225f120d
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-05 16:15:57 -0700

    Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Dog
+
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Hug
+# Kiss
+# Loyal
