de0c809a545f5f02f74877df7faeb8d7eacee70e
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-05 20:10:03 -0700

    indenting the code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,17 +5,17 @@ class Puppy
 		toy
 	end
 
-# A speak method that takes an integer, and then prints "Woof!" that many times
+	# A speak method that takes an integer, and then prints "Woof!" that many times
 	def speak(number)
 		puts 'Woof!' * number
 	end
 
-# Aa roll_over method that just prints "*rolls over*"
+	# Aa roll_over method that just prints "*rolls over*"
 	def roll_over
 		p "*rolls over*"
 	end
 
-# A dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
+	# A dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
 	def dog_years(human_years)
 		(human_years / 7.00).round
 	end
@@ -35,38 +35,38 @@ end
 # I stored the data in a hash then called each to print only the addition calculations.
 
 class Calculator
-  
-  def addition(int1, int2) 
-  	calculation = {}
-    puts "#{int1} + #{int2} = #{int1 + int2}"
-    calculation["#{int1} + #{int2} ="] = int1 + int2
-    calculation
-  end
-  
-  def subtraction(int1, int2) 
-  	calculation = {}
-    puts "#{int1} - #{int2} = #{int1 - int2}"
-    calculation["#{int1} - #{int2} ="] = int1 - int2
-    calculation
-  end
-  
-  def multiplication(int1, int2) 
-  	calculation = {}
-    puts "#{int1} * #{int2} = #{int1 * int2}"
-    calculation["#{int1} * #{int2} ="] = int1 * int2
-    calculation
-  end
-  
-  def division(int1, int2) 
-  	calculation = {}
-    puts "#{int1} / #{int2} = #{(int1 / int2.to_f).round(2)}"
-    calculation["#{int1} / #{int2} ="] = (int1 / int2.to_f).round(2)
-    calculation
-  end
-  
-  def initialize
+
+	def addition(int1, int2) 
+		calculation = {}
+		puts "#{int1} + #{int2} = #{int1 + int2}"
+		calculation["#{int1} + #{int2} ="] = int1 + int2
+		calculation
+	end
+
+	def subtraction(int1, int2) 
+		calculation = {}
+		puts "#{int1} - #{int2} = #{int1 - int2}"
+		calculation["#{int1} - #{int2} ="] = int1 - int2
+		calculation
+	end
+
+	def multiplication(int1, int2) 
+		calculation = {}
+		puts "#{int1} * #{int2} = #{int1 * int2}"
+		calculation["#{int1} * #{int2} ="] = int1 * int2
+		calculation
+	end
+
+	def division(int1, int2) 
+		calculation = {}
+		puts "#{int1} / #{int2} = #{(int1 / int2.to_f).round(2)}"
+		calculation["#{int1} / #{int2} ="] = (int1 / int2.to_f).round(2)
+		calculation
+	end
+
+	def initialize
 		puts "Initializing a random calculation instance ..."
-  end
+	end
 
 end
 
@@ -75,29 +75,29 @@ repeat = 50
 calculation_history = {}
 until repeat == 0 
 
-calculation = ["+", "-", "*", "/"]
-oeration = calculation[rand(calculation.length)]
+	calculation = ["+", "-", "*", "/"]
+	oeration = calculation[rand(calculation.length)]
 
-if oeration == '+'
-  calculation_history = calculation_history.merge(Calculator.new.addition(rand(100),rand(100)))
-elsif oeration == '-'
-  calculation_history = calculation_history.merge(Calculator.new.subtraction(rand(100),rand(100)))
-elsif oeration == '*'
-  calculation_history = calculation_history.merge(Calculator.new.multiplication(rand(100),rand(100)))
-elsif oeration == '/'
-  calculation_history = calculation_history.merge(Calculator.new.division(rand(100),rand(100)))
+	if oeration == '+'
+		calculation_history = calculation_history.merge(Calculator.new.addition(rand(100),rand(100)))
+	elsif oeration == '-'
+		calculation_history = calculation_history.merge(Calculator.new.subtraction(rand(100),rand(100)))
+	elsif oeration == '*'
+		calculation_history = calculation_history.merge(Calculator.new.multiplication(rand(100),rand(100)))
+	elsif oeration == '/'
+		calculation_history = calculation_history.merge(Calculator.new.division(rand(100),rand(100)))
+	end
+
+	repeat -= 1 
+
 end
 
-repeat -= 1 
 
-end
-  
-  
 puts 'Addition operations history:'
 calculation_history.each do |calculation , result |
 	if calculation.split(' ')[1] == '+'
 		puts "#{calculation} #{result}"
-end
+	end
 end
 
 
@@ -113,20 +113,5 @@ Puppy.new.roll_over
 p Puppy.new.dog_years(34)
 
 Puppy.new.jump
-
-
-# Without changing your driver code, run the program. Does initialize run? When?
-
-
-# In puppy_methods.rb, design and implement your own class below the Puppy class -- anything you'd like, 
-# but it should have an initialize method and at least two other instance methods. Then do the following:
-# Use a loop to make 50 instances of your class.
-# Modify your loop so that it stores all of the instances in a data structure.
-# Iterate over that data structure using .each and call the instance methods you wrote on each instance.
-# So if you wrote a Gymnast class, this is where you'd call .flip and .jump on each of your instances of Gymnast.
-# If the steps above feel really unfamiliar, remember that you've been working with classes the entire time --
-# strings are instances of the String class. So you can start by using string instances in place of your own class instances,
-# if that helps you visualize how the code should come together.
-
 
 
