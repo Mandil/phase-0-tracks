cc234def5aecabf51acb21fee122bf162a1ada81
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-05 17:02:37 -0700

    Release 0: Add Behavior to a Class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,40 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
+
+# A speak method that takes an integer, and then prints "Woof!" that many times
+	def speak(number)
+		puts 'Woof!' * number
+	end
+
+# Aa roll_over method that just prints "*rolls over*"
+	def roll_over
+		p "*rolls over*"
+	end
+
+# A dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
+	def dog_years(human_years)
+		(human_years / 7.00).round
+	end
+
+	def jump
+		puts "Bow-wow!"
+	end
 
 end
+
+
+Puppy.new.fetch("ball")
+# Driver code that initializes an instance of Puppy, and verify that the instance can now fetch a ball
+
+Puppy.new.speak(99)
+
+Puppy.new.roll_over
+
+Puppy.new.dog_years(34)
+
+Puppy.new.jump
 
