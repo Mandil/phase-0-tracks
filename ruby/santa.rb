87e06608b953a2c99a124fbe17ae864a2fa6219c
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-07 15:12:29 -0700

    Santa simulation code is complete

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,57 +1,42 @@
 class Santa
 
-  attr_reader :ethnicity
-  attr_accessor  :gender
-
-# age, which is not passed in on initialization and defaults to 0
-
-# A method that will print "Ho, ho, ho! Haaaappy holidays!"
+  attr_reader :ethnicity, :gender
+  
+ 	def initialize(gender, ethnicity)
+ 		puts "Initializing Santa instance ..."
+ 		@gender = gender
+ 		@ethnicity = ethnicity
+ 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"].sample
+ 		@age = rand(140)
+ 		puts "Santa name: #{@reindeer_ranking}/ gender: #{@gender}/ ethnicity: #{@ethnicity}/ #{@age} years old"
+ 	end
+ 	
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end 
 	
-# An method that takes a cookie type as a parameter and prints "That was a good <cookie type>!" 
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
 	
-# An initialize method that prints "Initializing Santa instance ..."
- 	def initialize
- 		puts "Initializing Santa instance ..."
- 	end
-	
 	def celebrate_birthday
-		santa_age = 1 
+		@age += 1 
 	end
 	
-	def get_mad_at(reindeer, reindeer_ranking)
-		index = reindeer_ranking.index(reindeer)
-		swap = reindeer_ranking[index]
-		reindeer_ranking.delete_at(index)
-		reindeer_ranking.push(swap)
-		reindeer_ranking
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.push(reindeer)
 	end
 	
 end
 
 
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+1000.times {Santa.new(example_genders.sample, example_ethnicities.sample)}
 
-# santas = []
-# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-# example_genders.length.times do |i|
-#   santas << Santa.new(example_genders[i], example_ethnicities[i])
-# end
-# p santas
 
 # Santa.new.eat_milk_and_cookies('snickerdoodle')
 # Santa.new.speak
 
-Santa.new.get_mad_at("Vixen", reindeer_ranking)
-p reindeer_ranking
-
-
-
-
-\
+# Santa.new.get_mad_at("Vixen")
