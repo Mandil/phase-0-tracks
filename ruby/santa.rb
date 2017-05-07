b4d4dde9e2c89c53ff335234beecb72af3b906fe
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-07 10:29:33 -0700

    Release 1: Give Santa Attributes for Christmas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,8 @@
 class Santa
 
+
+# age, which is not passed in on initialization and defaults to 0
+
 # A method that will print "Ho, ho, ho! Haaaappy holidays!"
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -11,12 +14,31 @@ class Santa
 	end
 	
 # An initialize method that prints "Initializing Santa instance ..."
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
 	end
 	
 end
 
-Santa.new.eat_milk_and_cookies('orios')
 
-Santa.new.speak
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+p santas
+
+# Santa.new.eat_milk_and_cookies('snickerdoodle')
+# Santa.new.speak
+
+
+
+
+
+
+
