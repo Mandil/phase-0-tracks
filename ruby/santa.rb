f5d69f5842ab20ea1f0ef90694c1a1db4b4aa976
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-07 11:29:02 -0700

    Release 2: Change an Attribute with a Method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,7 @@
 class Santa
 
+  attr_reader :ethnicity
+  attr_accessor  :gender
 
 # age, which is not passed in on initialization and defaults to 0
 
@@ -14,10 +16,20 @@ class Santa
 	end
 	
 # An initialize method that prints "Initializing Santa instance ..."
-	def initialize(gender, ethnicity)
-		puts "Initializing Santa instance ..."
-		@gender = gender
-		@ethnicity = ethnicity
+ 	def initialize
+ 		puts "Initializing Santa instance ..."
+ 	end
+	
+	def celebrate_birthday
+		santa_age = 1 
+	end
+	
+	def get_mad_at(reindeer, reindeer_ranking)
+		index = reindeer_ranking.index(reindeer)
+		swap = reindeer_ranking[index]
+		reindeer_ranking.delete_at(index)
+		reindeer_ranking.push(swap)
+		reindeer_ranking
 	end
 	
 end
@@ -25,20 +37,21 @@ end
 
 reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
-p santas
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
 
 # Santa.new.eat_milk_and_cookies('snickerdoodle')
 # Santa.new.speak
 
+Santa.new.get_mad_at("Vixen", reindeer_ranking)
+p reindeer_ranking
 
 
 
 
-
-
+\
