46a2043eab7cdc99a0d37d6d946d9e05cb4b5e90
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-11 03:54:47 -0700

    condition added

--- js/explore.js
+++ js/explore.js
@@ -9,10 +9,15 @@
 
 
 function reverse(string) {
-    var new_string = "";
-    for (var i = string.length - 1; i >= 0; i--) {
-        new_string += string[i];
-    }
-    return new_string;
+	var new_string = "";
+	if (string.length < 12) {
+		for (var i = string.length - 1; i >= 0; i--) {
+			new_string += string[i];
+		}
+		return new_string;
+	}
 }
-reverse('hello');
+new_word = reverse('hello world');
+
+console.log(new_word);
+console.log(reverse('welcome back mark'));
