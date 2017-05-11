c7844ee72186d7314aeac427ecdfe507b6f248d5
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-11 03:43:32 -0700

    initial code works

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,16 @@
 // create empty string "new_string" that will take the new created string
 // loop from (string.length - 1)  to 0, —-i
 // new_string += string[i]
+
+
+// Program: 
+
+
+function reverse(string) {
+    var new_string = "";
+    for (var i = string.length - 1; i >= 0; i--) {
+        new_string += string[i];
+    }
+    return new_string;
+}
+reverse('hello');
