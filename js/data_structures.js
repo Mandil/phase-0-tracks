7d4921b48ac91d6066dc57a410aa8ae3f3f7daec
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-13 16:58:42 -0700

    making function that create cars with few different properties of various data types, including function

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,3 +10,22 @@ for (i=0; i < colors.length; i++) {
 	barn_horses[horse_names[i]] = colors[i];
 }
 console.log(barn_horses);
+
+
+//
+
+function Cars(make, color, mpg, automatic){
+  this.make = make;
+  this.color = color;
+  this.mpg = mpg;
+  this.automatic = automatic;
+  this.drive = function () {console.log('*vroom*');}
+}
+
+var my_car = new Cars('Ford', 'Raspberry', 43, true);
+my_car.drive();
+
+console.log(my_car);
+
+console.log(new Cars('Volkswagen', 'Navy Blue', 28, true));
+console.log(new Cars('Chevy', 'Silver', 19, false));
