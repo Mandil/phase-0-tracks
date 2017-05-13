bc6f18070b6289ed019d2923ea3c359473329a37
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-13 16:46:27 -0700

    creating a hash from 2 arrays one being keys and the other values

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,3 +2,11 @@ var colors = ['blue', 'red', 'raspberry', 'yellow'];
 var horse_names = ['Ed', 'raspberry', 'runner', 'sea biscuit'];
 colors.push('white');
 console.log(colors);
+
+colors.splice(1,1)
+console.log(colors);
+var barn_horses = {};
+for (i=0; i < colors.length; i++) {
+	barn_horses[horse_names[i]] = colors[i];
+}
+console.log(barn_horses);
