b6c23c2ee39b6376a606fc5bb8c05696d2d1273b
Eleanor Roosevelt <mandil@Hassans-MacBook-Pro.local>
2017-05-13 23:45:09 -0700

    creating isSimilar function which will check 2 hashs and will return true if any key-value pair in the 2 hashes are the same

--- js/algos.js
+++ js/algos.js
@@ -8,8 +8,19 @@ function longestPhrase(array){
 		}
 		return phrase
 	}
+function isSimilar(hash1,hash2) {
+	var similar = false;
+	for (var key1 in hash1) {
+			for (var key2 in hash2) {
+				if ((key1 === key2) && (hash1[key1] === hash2[key2])) {
+					similar = true;}
+			}
+	}
+	return similar;
+}
 
 //// Driver code \\\\
+// longest phrase 
 
 var array = ["long phrase","longest phrase","longer phrase"];
 	
@@ -22,3 +33,25 @@ console.log(longestPhrase(colors));
 var horse_names = ['Ed', 'raspberry', 'runner', 'sea biscuit'];
 
 console.log(longestPhrase(horse_names));
+
+
+// similar objects
+
+var Object1 = {name: "Steven", age: 54};
+var Object2 = {name: "Tamir", age: 54};
+var Object3 = {animal: "Dog", legs: 4};
+var Object4 = {animal: "Dog", legs: 3};
+
+
+console.log('Is', Object1 , 'similar to' , Object2 , '?');
+console.log(isSimilar(Object1,Object2));
+
+console.log('Is' , Object3 , 'similar to' , Object2 , '?');
+console.log(isSimilar(Object3,Object2));
+
+console.log('Is' , Object2 , 'similar to ' , Object4 , '?');
+console.log(isSimilar(Object2,Object4));
+
+console.log('Is' , Object3 , 'similar to' , Object4 , '?');
+console.log(isSimilar(Object3,Object4));
+
